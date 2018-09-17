b99a262129f153b472fda70714bd323b1c05be14
Darcie <darciewalsh@hotmail.com>
2018-09-17 20:30:59 +0100

    Add cat route and insert image

--- app.rb
+++ app.rb
@@ -7,3 +7,13 @@ end
 get '/secret' do
   'TOP SECRET'
 end
+
+get '/dogs' do
+  'Dogs are better than cats'
+end
+
+get '/cat' do
+  "<div style='border: 3px dotted red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
