ce16ccf826308273ffe7d6fca58adcf51594c30e
Darcie <darciewalsh@hotmail.com>
2018-09-18 11:49:57 +0100

    move form html to sperate erb

--- app.rb
+++ app.rb
@@ -12,8 +12,17 @@ get '/dogs' do
   'Dogs are better than cats'
 end
 
-get '/cat' do
-  "<div style='border: 3px dotted red'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
 end
